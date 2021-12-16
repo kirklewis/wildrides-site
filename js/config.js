a366c667da91dbcb7c1ce0d6f5bfcd09a86fb6b2
Kirk Lewis <kirkcodes@yahoo.com>
2021-12-16 23:15:53 +0000

    Added JS Config
    This change adds the cognito user pool id and clientID.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_bpEwVzTGk',
+        userPoolClientId: 'u1hm6g80n75kidd0kpssc3abk',
+        region: 'eu-west-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
